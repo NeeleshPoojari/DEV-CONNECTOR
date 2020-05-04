a85ef3c228e717a37d3a1f3efd303666ff207e84
neepooja <neepooja@publicisgroupe.net>
2020-05-05 01:39:55 +0530

    Added Profile model and route to get logged in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,33 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
-// @route  GET api/profile
-//@desc Test Route
+// @route  GET api/profile/me
+//@desc   Get current user
 //@access Public
 
-router.get('/', (req,res) => res.send('U reached jupiter'))
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', [
+            'name',
+            'avatar'
+        ]);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).send('Server Error');
+    }
+    res.json(profile);
+
+
+});
 
 module.exports = router;
