9d7c1530d424a81e87fd931e8edd6f77387342a5
neepooja <neepooja@publicisgroupe.net>
2020-07-28 12:23:41 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const connectDB = require('./config/db');
 
@@ -12,10 +13,6 @@ app.use(express.json({
     extended: false
 }));
 
-
-
-app.get('/', (req,res) => res.send('API Running'));
-
 const PORT = process.env.PORT || 5000;
 
 //Routes
@@ -23,6 +20,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    } )
+}
 
 app.listen(PORT,() => console.log(`Running on Port ${PORT}`));
 
