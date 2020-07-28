505cbdab73d294a41b904df96f5281016115668e
neepooja <neepooja@publicisgroupe.net>
2020-07-28 18:19:19 +0530

    Avatar fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,7 +43,7 @@ router.post(
           .json({ errors: [{ msg: "User already exists " }] });
       }
       //User Avatar
-      const avatar = gravatar.url({
+      const avatar = gravatar.url( email,{
         s: "200",
         r: "pg",
         d: "mm",
